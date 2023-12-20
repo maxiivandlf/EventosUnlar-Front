7590a98ce7f25b4b138d0d52bd230ca5459998b9
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2023-12-19 23:49:31 -0300

    add header and grid events page

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
 import NavHeader from './navHeader/NavHeader';
-import CardEvent from './CardEvent';
+import MediaCard from './CardEvent';
 import MapView from './maps/MapView';
 import Header from './Header';
 import Button from './button/Button';
+import ResponsiveAppBar from './MenuAppBar';
 
-export { NavHeader, CardEvent, MapView, Header, Button };
+export { NavHeader, MediaCard, MapView, Header, Button, ResponsiveAppBar };
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ import encuentro from '../../assets/encuentro_informatico.jpg';
 import MediaCard from '../../components/CardEvent';
 import * as getEventsThunk from '../../redux/thunks/thunks';
 import { useDispatch, useSelector } from 'react-redux';
-import { Skeleton, Pagination, Grid } from '@mui/material';
+import { Box, CircularProgress, Pagination, Grid } from '@mui/material';
 
 const handleClick = () => {
   console.log('Ver mas detalles del evento');
@@ -52,19 +52,16 @@ function Landing() {
         <h2 style={{ color: 'white' }}>Proximos eventos</h2>
         <Grid container gap={3} alignItems={'center'} justifyContent={'center'}>
           {isLoading ? (
-            <Grid item xs={6} md={4}>
-              <Skeleton variant='text' sx={{ fontSize: '1rem' }} />
-              <Skeleton variant='circular' width={40} height={40} />
-              <Skeleton variant='rectangular' width={210} height={60} />
-              <Skeleton variant='rounded' width={210} height={60} />
-            </Grid>
+            <Box sx={{ display: 'flex' }}>
+              <CircularProgress />
+            </Box>
           ) : (
             events.map((event) => (
               <Grid key={event._id} item xs={6} md={4}>
                 <MediaCard
                   description={event.description}
                   title={event.name}
-                  imageURl={encuentro}
+                  imageURl={event.imageURL || '/default.jpg'}
                   idEvent={event._id}
                 />
               </Grid>
