ca10d5ed670a7be984824837359dd88273fed50c
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2023-12-20 23:34:45 -0300

    add new icon maker, add leyend , add props width map view

--- src/components/maps/Markers.jsx
+++ src/components/maps/Markers.jsx
@@ -1,36 +1,11 @@
 import { Marker, Popup } from 'react-leaflet';
-
-const markers = [
-  {
-    name: 'Oficina de Alumnos',
-    position: {
-      lat: -29.430422,
-      lng: -66.868745,
-    },
-    icon: '/vite.svg',
-  },
-  {
-    name: 'Comedor Universitario',
-    position: {
-      lat: -29.429487,
-      lng: -66.871146,
-    },
-    icon: '/vite.svg',
-  },
-  {
-    name: 'Biblioteca',
-    position: {
-      lat: -29.429927,
-      lng: -66.86888,
-    },
-    icon: '/vite.svg',
-  },
-];
+import { markers } from '../../constants/makerMap';
+import iconMarker from '../../assets/iconMaker';
 
 function Markers() {
   return markers.map((marker) => {
     return (
-      <Marker key={marker.name} position={marker.position}>
+      <Marker key={marker.name} position={marker.position} icon={iconMarker}>
         <Popup>{marker.name}</Popup>
       </Marker>
     );
